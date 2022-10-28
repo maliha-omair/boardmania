6d5bd209ef22f1949c9cc63e4d64af19ab213283
Maliha Omair <maliha@malihaomair.com>
2022-10-28 11:21:25 -0400

    added condition to show join group only if it is not owned by you

--- react-app/src/components/PublicRooms/PublicRoom.js
+++ react-app/src/components/PublicRooms/PublicRoom.js
@@ -1,7 +1,10 @@
 import styles from "../PublicRooms/PublicRooms.module.css"
 import defaultImage from "../../assets/1.png"
+import { useSelector } from "react-redux"
 
 export default function PublicRoom({room}) {
+    const user = useSelector(state => state.session.user);
+    console.log("user is ....",user)
     return room && (
         <div className={styles.innerDiv}>
             <div className={styles.imageDiv}>
@@ -16,7 +19,10 @@ export default function PublicRoom({room}) {
                 </div>
             </div>
             <div className={styles.joinButtonDiv}>
-                <button className={styles.joinButton}>Join</button>
+                {room.owner_id!=user.id &&  (
+                    <button className={styles.joinButton}>Join</button>
+                )}
+                
             </div>
         </div>
     )
