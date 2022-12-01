da4f447dd211fd9738bbd171efbc5307aec28c48
Maliha Omair <maliha@malihaomair.com>
2022-12-01 13:21:19 -0500

    added website flow

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -21,19 +21,24 @@ export default function SplashPage() {
                     BoardMania is an online Ludo game. You can play Ludo King in multiplayer mode. The first player to move all four pieces into their home triangle wins the game!
 
                     <h2>About Boarmania</h2>
-                    Boardmania is an online version of the classic board game Ludo. You can play with your friends and family, or against the computer. It’s a really easy game to learn, making it suitable for players of all ages.
+                    Boardmania is an online version of the classic board game Ludo. You can play with your friends and family. It’s a really easy game to learn, making it suitable for players of all ages.
 
                     <h3>Ludo Rules</h3>
-                    Ludo is a game played by two to four players. Each player must choose one of the four available colors to play as.
-
-                    The game starts with each player taking turns to roll the die. For a player to start moving a piece around the board, they must roll a six. Once you have multiple pieces in play, you can choose which piece you want to move.
-
-                    <h3>Rule of six</h3>
-                    If you have one or more pieces in play, you can choose whether to take a piece out or move a piece six spaces.
-                    Players that roll a six earn an additional roll.
-                    Occupied squares
-                    If your piece lands on a square occupied by an opponent, their piece goes back to their base, and they must roll a six to take it back out.
-                    If you land on the same square as one of your other pieces, it blocks the path for your opponents.
+                    To play game players need to join room and create game. 
+                    <li>Ludo is a game played by two players. </li>
+                    <li>The game starts with each player taking turns to roll the die. </li>
+                    <li>For a player to start moving a piece around the board, they must roll a six.</li>
+                    <li>Once you have multiple pieces in play, you can choose which piece you want to move.</li>
+                    <h3>Public Rooms</h3>
+                    <li>Players can find public rooms by clicking public rooms and then join them. </li>
+                    <li>Once player's membership is accepted they can create game. </li>
+                    <h3>Your Rooms</h3>
+                    <li>User can view their room and rooms which they have membership under your rooms</li>
+                    <li>User can delete or edit rooms</li>
+                    <h3>Game Viewers</h3>
+                    Ludo needs two players to start the game. Other users can join as a viewer by clicking the small eye next to on going game. 
+                    <h3>Membership</h3>
+                    User can accept or reject(delete) membership request by other players on rooms created by them.
                 </div>
                 <div className={styles.imageDiv}>
                     <img className={styles.image} src={image}></img>
