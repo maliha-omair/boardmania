90e0a2e46e10bbe01fb2f9f6fe59a6cdacb09c15
Maliha Omair <maliha@malihaomair.com>
2022-12-05 01:55:00 -0500

    added progress watch

--- react-app/src/components/PublicRooms/PublicRooms.js
+++ react-app/src/components/PublicRooms/PublicRooms.js
@@ -4,14 +4,25 @@ import { useDispatch } from "react-redux"
 import { getPublicRoomsThunk } from "../../store/rooms";
 import styles from "../PublicRooms/PublicRooms.module.css"
 import PublicRoom from "./PublicRoom";
-
+import { Watch } from "react-loader-spinner";
 export default function PublicRooms() {
     const dispatch = useDispatch();
     const rooms = useSelector(state => state.rooms && state.rooms.publicRooms)
     useEffect(() => {
         dispatch(getPublicRoomsThunk())
     }, [dispatch])
-    if (!rooms) return null
+    if (!rooms) return (
+        <Watch
+            height="80"
+            width="80"
+            radius="48"
+            color="#4fa94d"
+            ariaLabel="watch-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+        />
+    )
     return rooms && (rooms.length>0 && (
         <div className={styles.mainContainer}>
             {/* <h1>Public Rooms</h1> */}
--- react-app/src/components/UserRooms/OwnedRooms.js
+++ react-app/src/components/UserRooms/OwnedRooms.js
@@ -6,28 +6,40 @@ import { getUserRoomsThunk } from "../../store/rooms";
 import { getUserMemberships } from "../../store/session";
 import styles from "../UserRooms/UserRooms.module.css"
 import UserRoom from "./UserRoom";
+import { Watch } from "react-loader-spinner";
 
 export default function OwnedRooms() {
     const dispatch = useDispatch();
     const history = useHistory();
     const rooms = useSelector(state => state.rooms && state.rooms.userRooms)
     useEffect(() => {
-          dispatch(getUserRoomsThunk());
+        dispatch(getUserRoomsThunk());
     }, [dispatch])
-    function handleClick(){
+    function handleClick() {
         history.push("/newRoom")
     }
-    if (!rooms) return null
+    if (!rooms) return (
+        <Watch
+            height="80"
+            width="80"
+            radius="48"
+            color="#4fa94d"
+            ariaLabel="watch-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+        />
+    )
     return rooms && (rooms.length > 0 && (
         <div className={styles.mainContainer}>
             {Object.values(rooms).map((room) => {
                 return <UserRoom key={room.id} room={room} />
-            })}         
+            })}
         </div>
     )) || (
-        <div className={styles.noRooms}>
-            You have created no rooms 
-            &nbsp;<button className={styles.joinButton} onClick={handleClick}> Create</button>
-        </div>
-    )
+            <div className={styles.noRooms}>
+                You have created no rooms
+                &nbsp;<button className={styles.joinButton} onClick={handleClick}> Create</button>
+            </div>
+        )
 }
